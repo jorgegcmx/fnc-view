0a2378b2ec537bfcb2966620a4933ab24f2ca894
Jorge Gonzalez Camacho <jorge.a.gonzalez.camacho@gmail.com>
2024-04-30 20:55:20 -0600

    se agrega busqueda

--- src/app/components/cursos/lista-cursos/lista-cursos.component.ts
+++ src/app/components/cursos/lista-cursos/lista-cursos.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { ServiceService } from '../../../services/profesionales/service.service';
 import { EMPTY, Observable, catchError } from 'rxjs';
 import { CommonModule, NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-lista-cursos',
   standalone: true,
-  imports: [CommonModule,NgFor],
+  imports: [CommonModule, NgFor, FormsModule],
   templateUrl: './lista-cursos.component.html',
   styleUrl: './lista-cursos.component.css'
 })
@@ -14,19 +15,31 @@ export class ListaCursosComponent implements OnInit {
 
   public DataCursos$!: Observable<any[]>;
   nombre!: string;
+  buscar!: string;
 
   constructor(private service: ServiceService) {
     this.nombre = '1';
+    this.buscar = '';
   }
 
-  ngOnInit(){
-    console.log(this.nombre);
-    
+  ngOnInit() {
     this.DataCursos$ = this.service.getCursos(this.nombre).pipe(catchError((error: string) => {
       console.log(error);
       return EMPTY;
-    }))
+    }));
+  }
 
+  buscarLista() {
+    this.DataCursos$ = this.service.getCursos(this.buscar).pipe(catchError((error: string) => {
+      console.log(error);
+      return EMPTY;
+    }));
+  }
+  buscarTodos() {
+    this.DataCursos$ = this.service.getCursos(this.nombre).pipe(catchError((error: string) => {
+      console.log(error);
+      return EMPTY;
+    }));
   }
 
 }
