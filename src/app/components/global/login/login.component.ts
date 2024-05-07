b9160c5d5c8c9420836e96cf348b63c954180d3a
Jorge Gonzalez Camacho <jorge.a.gonzalez.camacho@gmail.com>
2024-05-07 11:59:28 -0600

    ajuste router link

--- src/app/components/global/login/login.component.ts
+++ src/app/components/global/login/login.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { LoginService } from '../../../services/login/login.service';
-import { Router } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
